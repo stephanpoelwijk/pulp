58d2f2ebf1bad154afa14ebca4cfb8ee6459ae89
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2023-02-17 18:32:35 +0100

    Derive content type from file extension

--- src/Pulp.WebApi/Extensions/FileSystem/FileSystemExtension.cs
+++ src/Pulp.WebApi/Extensions/FileSystem/FileSystemExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper;
+using Microsoft.AspNetCore.StaticFiles;
 using System.Globalization;
 
 namespace Pulp.WebApi.Extensions.FileSystem;
@@ -6,11 +7,14 @@ namespace Pulp.WebApi.Extensions.FileSystem;
 public static class FileSystemExtension
 {
     private const string ConfigurationFileName = "config.csv";
+    private const string DefaultContentType = "application/octet-stream";
 
     record RouteDefinition(string Method, string Route, int StatusCode, string ResponseFileName);
     
     public static IEndpointRouteBuilder UseFileSystemExtensionEndpoints(this IEndpointRouteBuilder builder)
     {
+        var fileExtensionContentTypeProvider = new FileExtensionContentTypeProvider();
+
         var configuration = builder.ServiceProvider.GetRequiredService<FileSystemConfiguration>();
         var configurationFileName = Path.Combine(configuration.DataRootFolder, ConfigurationFileName);
         if (!File.Exists(configurationFileName))
@@ -38,18 +42,23 @@ public static class FileSystemExtension
                     }
                 }
 
+                if (!fileExtensionContentTypeProvider.TryGetContentType(contentsFileName, out var contentType))
+                {
+                    contentType = DefaultContentType;
+                }
+
                 switch (routeDefinition.Method)
                 {
                     case "GET":
                         builder.MapGet(routeDefinition.Route, () =>
                         {
-                            return Results.Text(File.ReadAllText(contentsFileName), "application/json", null, routeDefinition.StatusCode);
+                            return Results.Text(File.ReadAllText(contentsFileName), contentType, null, routeDefinition.StatusCode);
                         });
                         break;
                     case "POST":
                         builder.MapPost(routeDefinition.Route, () =>
                         {
-                            return Results.Text(File.ReadAllText(contentsFileName), "application/json", null, routeDefinition.StatusCode);
+                            return Results.Text(File.ReadAllText(contentsFileName), contentType, null, routeDefinition.StatusCode);
                         });
                         break;
                     case "DELETE":
